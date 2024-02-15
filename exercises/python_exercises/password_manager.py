51ca634dc86faa535b8c9587c34eab34eb9ec149
Abhishek Kumar <a8h1@Abhisheks-MacBook-Pro.local>
2024-02-14 20:35:23 -0800

    added passowrd manager code

--- exercises/python_exercises/password_manager.py
+++ exercises/python_exercises/password_manager.py
@@ -1,16 +1,48 @@
-pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("What is the master password? ")
+
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User :", user, "| Password :", fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account name: ')
+    pwd = input('Password: ')
+    
+
+    with open('password.txt', 'a') as f:
+        f.write(name+ "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
-    mode = input("Would you like to a new password or view existing ones (view, add, Q)? ").lower()
+    mode = input("Would you like to a new password or view existing ones (view, add) press q to quite. ").lower()
     if mode == 'q':
         break
         
     if mode == "view":
-        print()
+        view()
     elif mode == "add":
-        print()
+        add()
     else:
         print("Not a valid option")
